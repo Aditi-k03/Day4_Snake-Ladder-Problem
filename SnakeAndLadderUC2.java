02ab4bdd249803282429dac4cf004cf2828c7d9b
Aditi Khade <115809644+Aditi-k03@users.noreply.github.com>
2023-02-01 21:08:48 +0530

    Add files via upload

---
+++ SnakeAndLadderUC2.java
@@ -0,0 +1,13 @@
+package com.Java.snakeladder;
+
+public class SnakeAndLadderUC2 
+{
+    public static void main(String[] args) 
+	{
+        int singlePlayerValue = 0;
+        int player = 0;
+        int dice = 1+(int)(Math.random()*6);
+        System.out.println("Initial Player Position:- " + player);
+        System.out.println(dice);
+    }
+}
